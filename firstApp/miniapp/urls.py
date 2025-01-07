98e9d536a7ddb140f3adb4acd492ef70e46d96a2
Abhilash-03 <abhilashjha335@gmail.com>
2025-01-07 14:48:39 +0530

    django forms

--- firstApp/miniapp/urls.py
+++ firstApp/miniapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.all_app, name='all_app'),
     path('<int:chai_id>/', views.chai_detail, name='chai_detail'),
+    path('chai_store/', views.chai_store_view, name='chai_stores'),
 ]
